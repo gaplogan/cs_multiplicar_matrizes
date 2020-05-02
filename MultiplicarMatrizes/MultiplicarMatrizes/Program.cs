0133e19ccedbaede24763a4894e3449d500d0abc
gaplogan <gaplogan@gmail.com>
2020-05-02 20:50:09 -0300

    v.2020.05.02.20.50

---
+++ MultiplicarMatrizes/MultiplicarMatrizes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplicarMatrizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz1 = new int[2, 3];
+            int[,] matriz2 = new int[3, 2];
+            int[,] resultado = new int[2, 2];
+
+            matriz1[0, 0] = 5;
+            matriz1[0, 1] = 2;
+            matriz1[0, 2] = 3;
+            matriz1[1, 0] = 9;
+            matriz1[1, 1] = 4;
+            matriz1[1, 2] = 1;
+
+            matriz2[0, 0] = 4;
+            matriz2[0, 1] = 7;
+            matriz2[1, 0] = 5;
+            matriz2[1, 1] = 2;
+            matriz2[2, 0] = 2;
+            matriz2[2, 1] = 1;
+
+            resultado[0, 0] = (matriz1[0, 0] * matriz2[0, 0]) + (matriz1[0, 1] * matriz2[1, 0]) + (matriz1[0, 2] * matriz2[2, 0]);
+            resultado[1, 0] = (matriz1[1, 0] * matriz2[0, 0]) + (matriz1[1, 1] * matriz2[1, 0]) + (matriz1[1, 2] * matriz2[2, 0]);
+
+            resultado[0, 1] = (matriz1[0, 0] * matriz2[0, 1]) + (matriz1[0, 1] * matriz2[1, 1]) + (matriz1[0, 2] * matriz2[2, 1]);
+            resultado[1, 1] = (matriz1[1, 0] * matriz2[0, 1]) + (matriz1[1, 1] * matriz2[1, 1]) + (matriz1[1, 2] * matriz2[2, 1]);
+
+            Console.WriteLine("[" + resultado[0, 0] + "][" + resultado[0, 1] + "]");
+            Console.WriteLine("[" + resultado[1, 0] + "][" + resultado[1, 1] + "]");
+
+            Console.ReadKey();
+        }
+    }
+}
